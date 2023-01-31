0aeeacc54a2f8db727395b1480e759622286cdd1
rmob <riley.obryan@gmail.com>
2023-01-31 05:00:51 -0800

    removed commented out code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const express = require('express');
+
 const mysql = require('mysql2');
 const inquirer = require('inquirer')
 const figlet = require('figlet')
@@ -10,14 +10,6 @@ const questionAddEmployee = require('./routes/questionAddEmployee')
 const cTable = require('console.table');
 const questionUpdateEmployee = require('./routes/questionUpdateEmployee');
 require('dotenv').config()
-
-
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
 
 const db = mysql.createConnection(
   {
